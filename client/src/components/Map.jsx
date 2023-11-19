d3d3d71deb958be4f8b1251fb434b7f728dbe7b4
Rick <101254647+nauish@users.noreply.github.com>
2023-11-19 20:58:35 +0800

    feat: add autocomplete and pin results on map

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -3,21 +3,86 @@ import { Loader } from '@googlemaps/js-api-loader';
 
 const Map = () => {
   useEffect(() => {
-    const loader = new Loader({
-      apiKey: import.meta.env.VITE_MAPS_API,
-      version: 'weekly',
-    });
-
     const initMap = async () => {
-      const { Map } = await loader.importLibrary('maps');
-      const { Marker } = await loader.importLibrary('marker');
-
-      let map = new Map(document.getElementById('map'), {
-        center: { lat: -34.397, lng: 150.644 },
-        zoom: 8,
+      // instantiate a loader from official Google Maps js-api-loader library
+      const loader = new Loader({
+        apiKey: import.meta.env.VITE_MAPS_API,
+        version: 'weekly',
       });
 
-      new Marker({ position: map.getCenter(), map });
+      // import libraries in parallel instead of loading everything using load()
+      const [mapsLib, markerLib, placesLib] = await Promise.all([
+        loader.importLibrary('maps'),
+        loader.importLibrary('marker'),
+        loader.importLibrary('places'),
+      ]);
+
+      const { Map, InfoWindow } = mapsLib;
+      const { Marker } = markerLib;
+      const { Autocomplete, PlacesService } = placesLib;
+
+      const map = new Map(document.getElementById('map'), {
+        center: { lat: -34.397, lng: 150.644 },
+        zoom: 17,
+      });
+
+      const autocomplete = new Autocomplete(
+        document.getElementById('autocomplete'),
+      );
+
+      const marker = new Marker({
+        map: map,
+        position: { lat: -34.397, lng: 150.644 },
+      });
+
+      autocomplete.addListener('place_changed', () => {
+        marker.setVisible(false);
+        const place = autocomplete.getPlace();
+
+        console.log(place);
+
+        if (!place.geometry) {
+          alert("Autocomplete's returned place contains no geometry");
+          return;
+        }
+
+        if (place.geometry.viewport) map.fitBounds(place.geometry.viewport);
+        else map.panTo(place.geometry.location);
+
+        marker.setPosition(place.geometry.location);
+        marker.setVisible(true);
+
+        const request = {
+          location: place.geometry.location,
+          radius: '500',
+        };
+
+        const service = new PlacesService(map);
+        service.nearbySearch(request, (results, status) => {
+          console.log(results);
+          const createMarker = (place) => {
+            const marker = new Marker({
+              map: map,
+              position: place.geometry.location,
+            });
+            marker.addListener('click', () => {
+              infoWindow.setContent(place.name);
+              infoWindow.open(map, marker);
+            });
+          };
+          if (status === 'OK') {
+            results.forEach((result) => {
+              const place = result;
+              createMarker(place);
+            });
+          }
+        });
+
+        let infoWindow = new InfoWindow({
+          content: place.name,
+        });
+        infoWindow.open(map, marker);
+      });
     };
 
     initMap();
@@ -25,6 +90,12 @@ const Map = () => {
 
   return (
     <div>
+      <input
+        id="autocomplete"
+        type="text"
+        placeholder="Search"
+        style={{ width: '100%' }}
+      />
       <div id="map" style={{ height: '400px', width: '100%' }}></div>
     </div>
   );
