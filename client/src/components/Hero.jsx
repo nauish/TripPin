e45c85dfd5f0ee07c7cb1aaff1c83390c691ffa8
Rick <101254647+nauish@users.noreply.github.com>
2023-12-13 23:15:08 +0800

    refactor: refactor Hero and Trips components

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,28 +1,32 @@
 import { Link } from 'react-router-dom';
 import { Button } from './ui/button';
+import { FaChevronDown } from 'react-icons/fa';
 
 const Hero = () => {
   return (
-    <div>
-      <img
-        src="https://images.unsplash.com/photo-1682685796467-89a6f149f07a?q=80&w=2340&auto=format&fit=crop&ixid=M3w1MzE4NzB8MHwxfHNlYXJjaHw0fHx0cmlwfGVufDB8fHx8MTcwMTg1NDk2N3ww"
-        alt="Woman looking at the valley"
-        className="absolute w-full h-[94vh] object-cover -z-10"
-      />
-      <div className="flex flex-col justify-center items-center pt-20">
-        <h1 className="font-bold text-5xl mb-3 line-height-1 mb-md-3 col-12 col-md-10 col-xl-9">
-          想去什麼地方？
-        </h1>
-        <p className="text-lg mb-4">
-          在 TripPin 上快速建立、規劃及探索你的行程
-        </p>
-        <div className="flex space-x-4 mt-8">
-          <Button>
-            <Link to="/user/trips">開始規劃</Link>
-          </Button>
+    <>
+      <div className="relative w-full h-[78vh]">
+        <img
+          src="https://images.unsplash.com/photo-1682685796467-89a6f149f07a?q=80&w=2340&auto=format&fit=crop&ixid=M3w1MzE4NzB8MHwxfHNlYXJjaHw0fHx0cmlwfGVufDB8fHx8MTcwMTg1NDk2N3ww"
+          alt="Woman looking at the valley"
+          className="absolute w-full h-full object-cover"
+        />
+        <div className="absolute top-0 left-0 w-full h-full flex flex-col justify-start items-center pt-12">
+          <h1 className="font-bold text-5xl mb-3 line-height-1 mb-md-3 col-12 col-md-10 col-xl-9">
+            想去哪？
+          </h1>
+          <p className="text-lg mb-4">在 TripPin 上探索你的旅程</p>
+          <div className="flex space-x-4 mt-8">
+            <Button>
+              <Link to="/user/trips">開始規劃</Link>
+            </Button>
+          </div>
         </div>
       </div>
-    </div>
+      <div className="flex justify-center -mt-10">
+        <FaChevronDown className="text-white text-3xl animate-bounce" />
+      </div>
+    </>
   );
 };
 
--- client/src/components/Trips.jsx
+++ client/src/components/Trips.jsx
@@ -1,15 +1,28 @@
 import { Link } from 'react-router-dom';
-import { Button } from '@/components/ui/button';
+import { Player } from '@lottiefiles/react-lottie-player';
 import TripCard from './TripCard';
 
-const Trips = ({ trips }) => {
+const Trips = ({ trips, showPlayer, tripsName }) => {
   return (
     <>
+      <h1 className="py-10 text-3xl font-bold text-center">
+        {tripsName || '行程列表'}
+      </h1>
+      {trips.length === 0 && showPlayer && (
+        <>
+          <Player
+            autoplay
+            loop
+            src="https://lottie.host/2673a739-ef57-45e6-bfde-cd212854bd8d/C0WkuiADG2.json"
+            className="w-1/3 mx-auto"
+          ></Player>
+          <h2 className="text-xl text-center">
+            您還沒有任何行程，快來新增吧！
+          </h2>
+        </>
+      )}
       <div className="flex justify-center pb-10">
         <div className="flex flex-col">
-          <Link to="/trip">
-            <Button className="w-20 my-4">新增行程</Button>
-          </Link>
           <div className="grid grid-cols-3 gap-4 max-w-5xl">
             {trips.map((trip) => (
               <Link to={`/trips/${trip.id}`} key={trip.id}>
