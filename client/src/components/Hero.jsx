0996f453df27ea49d1e7223edfce0584cce9408c
Rick <101254647+nauish@users.noreply.github.com>
2023-12-17 10:46:23 +0800

    update: add and delete media files, update Hero component

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,30 +1,39 @@
 import { Link } from 'react-router-dom';
 import { Button } from './ui/button';
-import { FaChevronDown } from 'react-icons/fa';
+import { ChevronDown } from 'lucide-react';
 
 const Hero = () => {
   return (
     <>
-      <div className="relative w-full h-[78vh]">
-        <img
-          src="https://images.unsplash.com/photo-1682685796467-89a6f149f07a?q=80&w=2340&auto=format&fit=crop&ixid=M3w1MzE4NzB8MHwxfHNlYXJjaHw0fHx0cmlwfGVufDB8fHx8MTcwMTg1NDk2N3ww"
-          alt="Woman looking at the valley"
-          className="absolute w-full h-full object-cover"
-        />
-        <div className="absolute top-0 left-0 w-full h-full flex flex-col justify-start items-center pt-12">
-          <h1 className="font-bold text-5xl mb-3 line-height-1 mb-md-3 col-12 col-md-10 col-xl-9">
-            想去哪？
-          </h1>
-          <p className="text-lg mb-4">在 TripPin 上探索你的旅程</p>
+      <div className="relative top-0 w-full h-screen">
+        <video
+          autoPlay
+          poster="/placeholder.webp"
+          muted
+          loop
+          className=" w-full h-full object-cover"
+        >
+          <source src="/hero.mp4" type="video/mp4"></source>
+        </video>
+        <div className="absolute top-28 left-0 w-full h-full flex flex-col justify-start items-center pt-12">
+          <h1 className=" font-bold text-6xl mb-3">想去哪？</h1>
+          <p className=" text-xl mb-4">在 TripPin 上探索你的旅程</p>
           <div className="flex space-x-4 mt-8">
-            <Button>
-              <Link to="/user/trips">開始規劃</Link>
-            </Button>
+            <Link to="/trip">
+              <Button>開始規劃</Button>
+            </Link>
           </div>
         </div>
       </div>
-      <div className="flex justify-center -mt-10">
-        <FaChevronDown className="text-white text-3xl animate-bounce" />
+      <div className="flex justify-center -mt-20">
+        <ChevronDown
+          strokeWidth={4}
+          size={48}
+          className="text-white text-3xl animate-bounce cursor-pointer"
+          onClick={() =>
+            window.scrollTo({ top: window.innerHeight, behavior: 'smooth' })
+          }
+        />
       </div>
     </>
   );
