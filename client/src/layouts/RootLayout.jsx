d82d02cfb9e4efe6f64505ca37930b6b0cc2d93b
Rick <101254647+nauish@users.noreply.github.com>
2023-12-17 10:50:52 +0800

    chore: clean up RootLayout component

--- client/src/layouts/RootLayout.jsx
+++ client/src/layouts/RootLayout.jsx
@@ -1,23 +1,15 @@
 import { Outlet } from 'react-router-dom';
-import { SocketProvider } from '../context/SocketContext';
-import { Navbar } from '../components/Navbar';
 import { ToastContainer } from 'react-toastify';
+import { SocketProvider } from '../context/SocketContext';
+import Navbar from '../components/Navbar';
 import 'react-toastify/dist/ReactToastify.min.css';
 
 export default function RootLayout() {
   return (
     <SocketProvider>
-      <div className="root-layout min-h-screen relative pt-[64px]  overflow-hidden">
-        <header>
-          <Navbar />
-        </header>
-        <main className="">
-          <Outlet />
-        </main>
-        <>
-          <ToastContainer position="bottom-right" />
-        </>
-      </div>
+      <Navbar />
+      <Outlet />
+      <ToastContainer position="bottom-right" />
     </SocketProvider>
   );
 }
