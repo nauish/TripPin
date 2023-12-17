4f0f265ac6f4e6284e6f31fd96e7cef636838953
Rick <101254647+nauish@users.noreply.github.com>
2023-12-17 10:53:21 +0800

    feat: add sort functionality to trips page

--- client/src/pages/LatestTrips.jsx
+++ client/src/pages/LatestTrips.jsx
@@ -1,24 +1,52 @@
 import { useState, useEffect } from 'react';
 import Trips from '@/components/Trips';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
 
 const LatestTrips = () => {
   const [trips, setTrips] = useState([]);
-  const [nextPage, setNextPage] = useState(0);
+  const [sort, setSort] = useState('latest'); // ['latest', 'top-rated', 'hottest']
+
+  const tripNames = {
+    latest: '最新行程',
+    'top-rated': '最高評價',
+    hottest: '最熱門',
+  };
 
   useEffect(() => {
-    fetch(
-      `${
-        import.meta.env.VITE_BACKEND_HOST
-      }api/v1/trips/latest?page=${nextPage}`,
-    )
+    fetch(`${import.meta.env.VITE_BACKEND_HOST}api/v1/trips?sort=${sort}`)
       .then((res) => res.json())
       .then((json) => {
         setTrips(json.data);
-        setNextPage(json.nextPage);
       });
-  }, []);
+  }, [sort]);
 
-  return <Trips trips={trips} tripsName="最新行程" />;
+  return (
+    <>
+      <div className="flex w-full flex-col items-center justify-center">
+        <div className="pt-28 -mb-16 max-w-xl">
+          <Select value={sort} onValueChange={setSort}>
+            <SelectTrigger className="w-[180px]">
+              <SelectValue placeholder="請選擇" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="latest">{tripNames.latest}</SelectItem>
+              <SelectItem value="top-rated">
+                {tripNames['top-rated']}
+              </SelectItem>
+              <SelectItem value="hottest">{tripNames.hottest}</SelectItem>
+            </SelectContent>
+          </Select>
+        </div>
+        <Trips trips={trips} tripsName={tripNames[sort]} />
+      </div>
+    </>
+  );
 };
 
 export default LatestTrips;
