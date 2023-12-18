dc34f5f6ddb7e4055b2ca3252115a68501d33578
Rick <101254647+nauish@users.noreply.github.com>
2023-12-18 23:55:02 +0800

    feat: add trip edit and delete

--- client/src/components/TripCard.jsx
+++ client/src/components/TripCard.jsx
@@ -9,12 +9,19 @@ import {
 } from '@/components/ui/card';
 
 import { Link } from 'react-router-dom';
+import TripOptionButton from './TripOptionButton';
 
-const TripCard = ({ trip }) => {
+const TripCard = ({ trip, showOptions }) => {
   return (
     <>
-      <Link to={`/trips/${trip.id}`}>
-        <Card className=" hover:bg-slate-100 group">
+      <Card className=" hover:bg-slate-100 group relative">
+        {showOptions && (
+          <TripOptionButton
+            trip={trip}
+            className="absolute right-2 top-2 bg-gray-200 p-2 rounded-md hover:bg-gray-300"
+          />
+        )}
+        <Link to={`/trips/${trip.id}`}>
           <img
             src={trip.photo}
             className="rounded-t-md object-cover h-48 w-96"
@@ -22,7 +29,15 @@ const TripCard = ({ trip }) => {
           ></img>
           <CardHeader>
             <div className="flex justify-between">
-              <div>{trip.destination}</div>
+              <div>
+                {trip.destination}
+                {trip.type ? (
+                  <span className="text-gray-500"> | {trip.type}</span>
+                ) : (
+                  ''
+                )}
+              </div>
+
               {formatBudget(+trip.budget)}
             </div>
           </CardHeader>
@@ -33,15 +48,15 @@ const TripCard = ({ trip }) => {
               {formatDate(trip.start_date)} - {formatDate(trip.end_date)}
             </CardDescription>
 
-            <CardDescription>{trip.note}</CardDescription>
+            <CardDescription>筆記：{trip.note}</CardDescription>
           </CardContent>
           <CardFooter>
             <CardDescription>
               {trip.privacy_setting === 'public' ? '公開行程' : '私人行程'}
             </CardDescription>
           </CardFooter>
-        </Card>
-      </Link>
+        </Link>
+      </Card>
     </>
   );
 };
