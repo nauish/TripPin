dbca0fb20987f5bd808da956614de2ddb5e53e93
Rick <101254647+nauish@users.noreply.github.com>
2023-12-25 16:04:39 +0800

    feat: update Auth with tabs

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -1,16 +1,22 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { toast } from 'react-toastify';
+import { toast } from 'sonner';
+import { Button } from './ui/button';
+import { Label } from './ui/label';
+import { Input } from './ui/input';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from './ui/tabs';
 
 const Auth = () => {
   const [name, setName] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [currentForm, setCurrentForm] = useState('login');
+
   const navigate = useNavigate();
 
   const handleFormSubmit = async (e, endpoint) => {
     e.preventDefault();
+    setIsLoading(true);
 
     try {
       const response = await fetch(
@@ -34,6 +40,7 @@ const Auth = () => {
       if (json.error) {
         return toast.error(json.error);
       }
+
       toast.success('帳號密碼登入成功！');
 
       const {
@@ -44,149 +51,117 @@ const Auth = () => {
       localStorage.setItem('user', JSON.stringify(user));
 
       navigate(-1);
-      setTimeout(() => {
-        navigate(0);
-      }, 500);
     } catch (error) {
-      console.log(error);
+      toast.error(error?.message || '登出時出錯了');
+    } finally {
+      setIsLoading(false);
     }
   };
 
-  const switchToRegister = () => {
-    setCurrentForm('register');
-  };
-
-  const switchToLogin = () => {
-    setCurrentForm('login');
-  };
-
   return (
-    <>
-      <div className="form-wrapper max-w-xl mx-auto">
-        {currentForm === 'register' && (
-          <>
-            <div className="bg-white p-10 rounded-lg xl:shadow-lg mt-10">
-              <h2
-                className="text-3xl font-bold mb-10 text-gray-800"
-                id="register-form"
-              >
-                註冊帳號
-              </h2>
-              <form
-                className="register space-y-reverse space-y-5"
-                onSubmit={(e) => handleFormSubmit(e, 'signup')}
-              >
-                <label className="text-gray-600" htmlFor="name">
-                  用戶名：{' '}
-                </label>
-                <input
-                  className="w-full border p-2 rounded-lg"
-                  type="text"
-                  id="reg-name"
-                  name="name"
-                  autoComplete="off"
-                  onChange={(e) => setName(e.target.value)}
-                  required
-                />
-                <label className="text-gray-600" htmlFor="email">
-                  Email:{' '}
-                </label>
-                <input
-                  className="w-full border p-2 rounded-lg"
-                  type="email"
-                  id="reg-email"
-                  name="email"
-                  autoComplete="off"
-                  onChange={(e) => setEmail(e.target.value)}
-                  required
-                />
-                <label className="text-gray-600" htmlFor="password">
-                  密碼：{' '}
-                </label>
-                <input
-                  className="w-full border p-2 rounded-lg"
-                  type="password"
-                  name="password"
-                  id="reg-password"
-                  onChange={(e) => setPassword(e.target.value)}
-                  required
-                />
-                <div className="flex justify-between">
-                  <button
-                    className="bg-black text-white py-2 px-4 rounded-lg hover:bg-black"
-                    type="submit"
-                    id="register-btn"
-                  >
-                    註冊
-                  </button>
-                  <button
-                    onClick={switchToLogin}
-                    className="text-blue-700 cursor-pointer"
-                  >
-                    已經有帳號了嗎？請登入
-                  </button>
-                </div>
-              </form>
-            </div>
-          </>
-        )}
+    <div className="flex items-center justify-center min-h-screen">
+      <Tabs
+        defaultValue="login"
+        className="form-wrapper max-w-xl mx-auto md:w-[500px]"
+      >
+        <TabsList className="grid w-full grid-cols-2">
+          <TabsTrigger value="login">登入</TabsTrigger>
+          <TabsTrigger value="register">註冊</TabsTrigger>
+        </TabsList>
+        <TabsContent value="register">
+          <div className="bg-white px-20 py-14 rounded-lg md:border md:shadow-lg">
+            <h2
+              className="text-3xl font-bold mb-4 text-gray-800"
+              id="register-form"
+            >
+              註冊帳號
+            </h2>
+            <form
+              className="register space-y-reverse space-y-5"
+              onSubmit={(e) => handleFormSubmit(e, 'signup')}
+            >
+              <Label htmlFor="name">用戶名：</Label>
+              <Input
+                className="w-full border p-2 rounded-lg"
+                type="text"
+                id="reg-name"
+                name="name"
+                autoCapitalize="none"
+                autoCorrect="off"
+                onChange={(e) => setName(e.target.value)}
+                required
+              />
+              <Label htmlFor="email">Email: </Label>
+              <Input
+                type="email"
+                id="reg-email"
+                name="email"
+                autoComplete="email"
+                onChange={(e) => setEmail(e.target.value)}
+                required
+                disabled={isLoading}
+              />
+              <Label htmlFor="password">密碼： </Label>
+              <Input
+                type="password"
+                name="password"
+                id="reg-password"
+                disabled={isLoading}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
 
-        {currentForm === 'login' && (
-          <>
-            <div className="bg-white p-10 rounded-lg xl:shadow-lg mt-10">
-              <h2
-                className="text-3xl font-bold mb-10 text-gray-800"
-                id="login-form"
-              >
-                登入 TripPin
-              </h2>
-              <form
-                className="login space-y-reverse space-y-5"
-                onSubmit={(e) => handleFormSubmit(e, 'login')}
-              >
-                <label className="text-gray-600" htmlFor="email">
-                  Email:{' '}
-                </label>
-                <input
-                  className="w-full border p-2 rounded-lg"
-                  type="email"
-                  name="email"
-                  id="login-email"
-                  required
-                  onChange={(e) => setEmail(e.target.value)}
-                />
-                <label className="text-gray-600" htmlFor="password">
-                  密碼：{' '}
-                </label>
-                <input
-                  className="w-full border p-2 rounded-lg"
-                  type="password"
-                  name="password"
-                  id="login-password"
-                  required
-                  onChange={(e) => setPassword(e.target.value)}
-                />
-                <div className="flex justify-between">
-                  <button
-                    className="bg-black text-white py-2 px-4 rounded-lg hover:bg-black"
-                    type="submit"
-                    id="login-btn"
-                  >
-                    登入
-                  </button>
-                  <button
-                    onClick={switchToRegister}
-                    className="text-blue-700 cursor-pointer"
-                  >
-                    沒有帳號嗎？請註冊
-                  </button>
-                </div>
-              </form>
-            </div>
-          </>
-        )}
-      </div>
-    </>
+              <Button disabled={isLoading} type="submit" id="register-btn">
+                註冊
+              </Button>
+            </form>
+          </div>
+        </TabsContent>
+        <TabsContent value="login">
+          <div className="bg-white px-20 py-14 rounded-lg md:border md:shadow-lg">
+            <h2
+              className="text-3xl font-bold mb-4 text-gray-800"
+              id="login-form"
+            >
+              登入 TripPin
+            </h2>
+            <form
+              className="login space-y-reverse space-y-5"
+              onSubmit={(e) => handleFormSubmit(e, 'login')}
+            >
+              <Label className="text-gray-600" htmlFor="email">
+                Email:{' '}
+              </Label>
+              <Input
+                type="email"
+                name="email"
+                autoCapitalize="none"
+                autoComplete="email"
+                autoCorrect="off"
+                id="login-email"
+                required
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <Label className="text-gray-600" htmlFor="password">
+                密碼：
+              </Label>
+              <Input
+                type="password"
+                name="password"
+                id="login-password"
+                required
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <div className="flex justify-between">
+                <Button disabled={isLoading} type="submit" id="login-btn">
+                  登入
+                </Button>
+              </div>
+            </form>
+          </div>
+        </TabsContent>
+      </Tabs>
+    </div>
   );
 };
 
