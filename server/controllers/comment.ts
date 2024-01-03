bfeef32df13de9601473ba0a206aeab2e035c422
Rick <101254647+nauish@users.noreply.github.com>
2024-01-03 13:56:37 +0800

    fix: fix a bug where try  didn't catch the bug

--- server/controllers/comment.ts
+++ server/controllers/comment.ts
@@ -22,16 +22,16 @@ export async function getComments(req: Request, res: Response) {
 
 export async function postComment(req: Request, res: Response) {
   const client = await pool.connect();
-  const { photos } = req.files as unknown as { photos: { filename: string; key: string }[] };
-
-  let photoFileNames: string[] = [];
-  if (process.env.NODE_ENV === 'production') {
-    photoFileNames = photos?.map((photo) => photo.key.split('/')[1]);
-  } else {
-    photoFileNames = photos?.map((photo) => photo.filename);
-  }
-
   try {
+    const { photos } = req.files as unknown as { photos: { filename: string; key: string }[] };
+
+    let photoFileNames: string[] = [];
+    if (process.env.NODE_ENV === 'production') {
+      photoFileNames = photos?.map((photo) => photo.key.split('/')[1]);
+    } else {
+      photoFileNames = photos?.map((photo) => photo.filename);
+    }
+
     await client.query('BEGIN');
     const commentId = await insertComment(req.body, client);
     if (photos && photos.length > 0) await insertCommentPhotos(commentId, photoFileNames, client);
